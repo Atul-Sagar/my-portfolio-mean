11e6ec9deb619709d94717e22894728441a52112
Atul Sagar <atulsagar55@gmail.com>
2025-04-07 15:46:27 +0530

    updated the skills section and about me section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,16 @@ import { MatCardModule } from "@angular/material/card";
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatSliderModule } from '@angular/material/slider';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
+
+
 
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [
     // RouterOutlet
     MatToolbarModule,
@@ -18,7 +24,11 @@ import { MatListModule } from '@angular/material/list';
     MatCardModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatSliderModule,
+    FormsModule,
+    NgFor,
+    ReactiveFormsModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
@@ -26,6 +36,23 @@ import { MatListModule } from '@angular/material/list';
 export class AppComponent {
   title = 'my-portfolio';
 
+
+  skills = [
+    { name: 'HTML', value: 5 },
+    { name: 'CSS', value: 5 },
+    { name: 'JavaScript', value: 5 },
+    { name: 'Angular', value: 5 },
+    { name: 'Node.js', value: 5 },
+    { name: 'Express.js', value: 4 },
+    { name: 'MySQL', value: 3 },
+    { name: '.NET', value: 3 },
+    { name: 'Python', value: 4 },
+    { name: 'Golang', value: 3 },
+    { name: 'Flutter', value: 3 },
+    { name: 'Leadership', value: 4 },
+    { name: 'Teamwork', value: 5 },
+    { name: 'Communication (Improving)', value: 3 }
+  ];
   showSidenav = false;
 
   toggleSidenav() {
