c5bab3ab1eb8a219726c0f2e175620cf86049037
Atul Sagar <atulsagar55@gmail.com>
2025-06-17 11:18:26 +0530

    Updated the contact section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,7 +28,6 @@ import { trigger, transition, style, state, animate } from '@angular/animations'
     MatListModule,
     MatSliderModule,
     FormsModule,
-    NgFor,
     ReactiveFormsModule,
     RouterModule,
     MatProgressBarModule,
